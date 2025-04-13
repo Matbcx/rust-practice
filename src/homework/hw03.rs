134bfc8f6ccdeb4041734fa525cf7fc127e0ad9d
Matbcx <129828860+Matbcx@users.noreply.github.com>
2025-04-13 14:24:27 +0300

    hw03.rs

--- src/homework/hw03.rs
+++ src/homework/hw03.rs
@@ -1,16 +1,19 @@
 fn main() {
-    const ENVELOPE_WIDTH: usize = 30;
-    const ENVELOPE_HEIGHT: usize = 14;
+    let w: usize = 28;
+    let h: usize = 10;
 
-    for y in 0..ENVELOPE_HEIGHT {
-        for x in 0..ENVELOPE_WIDTH {
-            let is_hor = y == 0 || y == ENVELOPE_HEIGHT - 1;
-            let is_ver = x == 0 || x == ENVELOPE_WIDTH - 1;
-            let is_diag1 = x == y * (ENVELOPE_WIDTH - 1) / (ENVELOPE_HEIGHT - 1);
-            let is_diag2 = x == (ENVELOPE_WIDTH - 1) - y * (ENVELOPE_WIDTH - 1) / (ENVELOPE_HEIGHT - 1);
+    for y in 0..h {
+        for x in 0..w {
+            let is_hor = y == 0 || y == h - 1;
+            let is_ver = x == 0 || x == w - 1;
+
+            let diag1_x = (y * (w - 1) + (h - 1) / 2) / (h - 1);
+            let diag2_x = ((h - 1 - y) * (w - 1) + (h - 1) / 2) / (h - 1);
+
+            let is_diag1 = x == diag1_x;
+            let is_diag2 = x == diag2_x;
 
             let to_show = is_hor || is_ver || is_diag1 || is_diag2;
-
             let sym = if to_show { '*' } else { ' ' };
             print!("{}", sym);
         }
